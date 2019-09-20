f7e354ab092e780f452ff71a55e323abf1e9c6bd
Annette Iglesias <iglesiasannette@gmail.com>
2019-09-20 15:57:58 -0500

    Class Method  Exercises and Bonus Exercises

--- src/JavaStringBonuses.java
+++ src/JavaStringBonuses.java
@@ -1,5 +1,4 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.Scanner;
 
 
@@ -26,12 +25,111 @@ public class JavaStringBonuses {
         //input - 12/01/1999
         //output - December 12, 1999
 
-//        Date date = new Date();
-//        SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd, yyyy");
+
+
+
+//        System.out.println("Enter a date MM/DD/YYYY");
 //
-//        System.out.println("Enter the date: ");
-//        String userInput = formatter.format(date);
-//        System.out.println(userInput);
+//        String dateInput = input.nextLine().trim();
+//
+//        String[] arrOfStr = dateInput.split("/", 3); // Split at each slash into an array of 3 substrings
+//
+//        String month = arrOfStr[0];
+//        String day = arrOfStr[1];
+//        String year = arrOfStr[2];
+//
+//        switch(month){
+//            case "01" :
+//                month = "January";
+//                break;
+//            case "02" :
+//                month = "February";
+//                break;
+//            case "03" :
+//                month = "March";
+//                break;
+//            case "04" :
+//                month = "April";
+//                break;
+//            case "05" :
+//                month = "May";
+//                break;
+//            case "06" :
+//                month = "June";
+//                break;
+//            case "07" :
+//                month = "July";
+//                break;
+//            case "08" :
+//                month = "August";
+//                break;
+//            case "09" :
+//                month = "September";
+//                break;
+//            case "10" :
+//                month = "October";
+//                break;
+//            case "11" :
+//                month = "November";
+//                break;
+//            case "12" :
+//                month = "December";
+//                break;
+//        }
+//
+//
+//        String output = month + " " + day + ", " + year;
+//        System.out.println(output);
+
+
+
+////
+//        ----BONUS 2
+//
+//        Create an application that allows the user to enter a sentence
+//        and tells them how many vowels and consonants were used.
+//
+//        Example:
+//
+//        input - "The hill are alive."
+//        output - 5 vowels and 10 consonants
+
+
+//----BONUS 3
+
+//        Create a command line calculator application.
+//
+//        The program should welcome the user, prompt them for a calculation,
+//        and give the result. The user should be prompted after each result is
+//        outputted if they want to enter another calculation. When they
+//        say no, give them an exit message. Allow the user first to specify
+//        how many operands they will enter. Do not worry about
+//        input validation (assume user enters correct data type).
+//
+//        Example program:
+//
+//...
+//
+//        WELCOME TO THE CALCULATOR APP!!! (program output)
+//
+//        Do you wish to enter a new calculation? (program output)
+//        yes (user inputs this)
+//
+//        How many operands will you calculate? (program output)
+//        2 (user inputs this)
+//
+//        Please enter a calculation: (program output)
+//        2 + 2 (user enters this or something like this)
+//        The output of 2 + 2 is 4. (program outputs this)
+//
+//        Do you wish to enter a new calculation? (program outputs this)
+//        no (user enters this)
+//
+//        GOODBYE!!!
+
+
+
+
 
 
 
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -14,10 +14,16 @@ public class MethodsExercises {
 //        System.out.println(multiplication(7, 5));
 //
 //        System.out.println(division(10, 2));
+
+//          System.out.println(modulus(10, 2));
 //
 //        getInteger(1, 10);
 
-       printFactorial(10);
+
+        factorial(5);
+
+//
+
 
 //
     }
@@ -56,9 +62,10 @@ public class MethodsExercises {
 //        return num1 / num2;
 //    }
 //
-    public static double modulus(double num1, double num2) {
-        return num1 % num2;
-    }
+//    public static double modulus(double num1, double num2) {
+//
+//        return num1 % num2;
+//    }
 
 
 //2. Create a method that validates that user input is in a certain range
@@ -94,26 +101,43 @@ public class MethodsExercises {
 //
 //    }
 
+    // ================== using a loop
 //    public static int getInteger(int min, int max) {
-//
-//        Scanner input = new Scanner(System.in);
-//
+//        Scanner sc = new Scanner(System.in);
 //        while (true) {
-//            System.out.println("Please enter a number between " + min + "and" + max + ":");
-//            if (input.hasNextInt()) {
-//                int userNum = input.nextInt();
-//                if (userNum >= min && userNum < -max) {
+//            System.out.println("Please enter a number between " + min + " and " + max);
+//            // the hasNextInt method will wait for input and check (return true) if valid
+//            if (sc.hasNextInt()) {
+//                int userNum = sc.nextInt(); // nextInt() can be used here instead of Integer.parseInt(sc.nextLine())
+//                if (userNum >= min && userNum <= max) {
 //                    return userNum;
 //                }
-//                System.out.println("number out of range");
+//                System.out.println("Number out of range!");
 //            } else {
-//                System.out.println("invalid input");
+//                System.out.println("Invalid input!");
 //            }
-//            input.nextLine();//needed to clear scanner of current input
+//            sc.nextLine(); // needed to clear scanner of current input
 //        }
-//
 //    }
 
+// ================== using recursion
+//    public static int getInteger(int min, int max) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter a number between " + min + " and " + max);
+//        if (sc.hasNextInt()) {
+//            int userNum = sc.nextInt();
+//            if (userNum >= min && userNum <= max) {
+//                return userNum;
+//            } else {
+//                System.out.println("Number out of range!");
+//                getInteger(min, max);
+//            }
+//        } else {
+//            System.out.println("Invalid input!");
+//            getInteger(min, max);
+//        }
+//        return 0;
+//    }
 
 //3. Calculate the factorial of a number.
 //
@@ -131,25 +155,29 @@ public class MethodsExercises {
 //2! = 1 x 2           = 2
 //3! = 1 x 2 x 3       = 6
 //4! = 1 x 2 x 3 x 4   = 24
+//
 
-    public static void printFactorial(int num) {
-        String multiples = "";
-        int factorial = 0;
-        String iteratorToString;
 
-        for (int i = 1; i <= num; i++) {
-            iteratorToString = Integer.toString(i);
-            if (i == 1) {
-                multiples = iteratorToString;
-                factorial = i;
-            }
-            if (i > 1) {
-                multiples += " x " + iteratorToString;
-                factorial = factorial * i;
-            }
-            System.out.printf("%4d!    %-60s = %d \n", i, multiples, factorial);
+    public static void factorial(int num) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = input.nextInt();
+        var result = 1; //as  we are starting at 1
+        var middleString = "";
+        for (var i = 1; i <= userInput; i++) {
+            result *= i; // i represents the corresponding loop iteration
+            middleString += " x "; // first loop is " x 1", then the second loop adds i,
+            middleString += "" + i;//so middleString become "1 x 2", etc.
+            System.out.format("\n%d! = %s = %d", i, middleString, result);
         }
 
     }
 }
+
+
+
+
+
+
+
 
