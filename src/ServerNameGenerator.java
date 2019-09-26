1331f7ee12aef0572fdda0160f63ba7b6e6eef72
Annette Iglesias <iglesiasannette@gmail.com>
2019-09-26 11:03:55 -0500

    revised to make more dynamic

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -14,17 +14,40 @@
 import java.util.Random;
     public class ServerNameGenerator {
 //
+        public static String[] nouns = {
+                "Daisy", "Rose", "Iris", "Orchid","Tulip","Sunflower","Carnation","Poppy","Cherry Blossom", "Zinnia",
+            };
+
+        public static String[] adj= {
+                "Aromatic", "Budding", "Charming", "Chic","Darling","Delicate","Elegant","Enchanting","Striking", "Supple",
+        };
+
+        //1A First version in randomization
+        public static Random r =  new Random(); ////using the random method from the Random class in java and assigning it to variable r
+
+
+        //2A second version in randomization that creates a method that allows you to pass in any array and/or arrays as parameters
+        public static String randomWords () {
+            Random random = new Random();
+            int ranNoun = random.nextInt(nouns.length);
+            int ranAdj = random.nextInt(adj.length);
+            return adj[ranAdj] + " " + nouns[ranNoun];
+        }
 
         public static void main(String[] args)
         {
-            String[] noun={"Daisy", "Rose", "Iris", "Orchid","Tulip","Sunflower","Carnation","Poppy","Cherry Blossom", "Zinnia",};
-            String[] adj={"Aromatic", "Budding", "Charming", "Chic","Darling","Delicate","Elegant","Enchanting","Striking", "Supple",};
-            Random r = new Random();
-            int randomNumber=r.nextInt(adj.length);
-            int randomNumber2=r.nextInt(noun.length);
+//             //2A first version in delivering randomization
+            int randomNumber=r.nextInt(adj.length); // generates a random index in the adj array
+            int randomNumber2=r.nextInt(nouns.length); //generates a random index in the nouns array
             System.out.print("You are as ");
             System.out.print(adj[randomNumber2] + " as a ");
-            System.out.println(noun[randomNumber] + ".");
+            System.out.println(nouns[randomNumber] + ".");
+
+            //2B second version
+//            System.out.print("You are an/a ");
+//            System.out.println(randomWords() + ".");
+
+
         }
 
     }
