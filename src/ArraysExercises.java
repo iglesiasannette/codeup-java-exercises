47b4ddf1f4f6316be3bf826bc20839583d240095
Annette Iglesias <iglesiasannette@gmail.com>
2019-09-26 10:28:02 -0500

    Revised Arrays Exercise to be more dynamic

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,16 +3,8 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-    //Create a static method named addPerson. It should accept an array of Person objects,
-    // as well as a single person object to add to the passed array.
-    // It should return an array whose length is 1 greater than the passed array,
-    // with the passed person object at the end of the array.
 
-    private static Person[] addPerson(Person[] people, Person newPeep) {
-        Person[] newGroup = Arrays.copyOf(people, people.length + 1);
-        newGroup[newGroup.length-1] = newPeep; //
-        return newGroup;
-    }
+
 
     public static void main(String[] args) {
 
@@ -25,11 +17,14 @@ public class ArraysExercises {
 
         //1. Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element.
         // Iterate through the array and print out the name of each person in the array.
-        Person[] peeps = new Person[3];
-        peeps[0] = new Person("Nancy");
-        peeps[1] = new Person("Tony");
-        peeps[2] = new Person("Bob");
-        Person[] people = {new Person("Nancy"), new Person("Tony"), new Person("Bob")};
+        Person[] people = {
+        new Person("Nancy"),
+        new Person("Tony"),
+        new Person("Bob"),
+
+        };
+        ;
+//        Person[] people = {new Person("Nancy"), new Person("Tony"), new Person("Bob")};
 
 //
         for (int i = 0; i < people.length; i += 1) {
@@ -47,16 +42,27 @@ public class ArraysExercises {
         }
 
 
-    //Create a static method named addPerson. It should accept an array of Person objects,
-        // as well as a single person object to add to the passed array.
-        // It should return an array whose length is 1 greater than the passed array,
-        // with the passed person object at the end of the array.
 
-        Person[] newGroup = addPerson(people, new Person("Delia")); //people is the array of person objects,
-        for (Person p : newGroup) {
-            System.out.println(p.getName());
+        //2B
+        people = addPerson(people, new Person("Delia")); //people is the array from the Person class,
+        people = addPerson(people, new Person("Sally"));
+
+
+        for (Person newPeep : people) {
+            System.out.println(newPeep.getName());
         }
 
+    }
+
+    //2A.Create a static method named addPerson. It should accept an array of Person objects,
+    // as well as a single person object to add to the passed array.
+    // It should return an array whose length is 1 greater than the passed array,
+    // with the passed person object at the end of the array.
+    //part 1
+    private static Person[] addPerson(Person[] people, Person newPeep) { //first parameter is the input of an array of person objects, the second parameter is a single person object, hence the reference to the class but not square brackets
+        Person[] newGroup = Arrays.copyOf(people, people.length + 1); //newGroup is the name of the new array
+        newGroup[newGroup.length-1] = newPeep; // [newGroup.length - 1] is the last index, which is thew newPeep added
+        return newGroup;
     }
 
 
